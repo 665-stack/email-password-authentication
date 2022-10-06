2c03ea58719d7fc29bdf31a3fe02b08e633bc199
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-06 12:06:53 +0600

    add onBlur in input field

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import app from './firebase.init';
 const auth = getAuth(app)
 
 function App() {
+
+  const handleEmailBlue = (event) => {
+    console.log(event.target.value)
+  }
+
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value)
+
+  }
   return (
     <div className="App">
       <form action="">
-        <input type="text" />
-        <input type="password" name="" id="" />
+        <input onBlur={handleEmailBlue} type="email" name="" id="" />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
       </form>
     </div>
   );
