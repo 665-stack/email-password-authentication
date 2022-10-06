474e65f136c3e173cdc4d6dccf02832bc7831573
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-06 13:01:58 +0600

    add boostrap form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
 
@@ -7,7 +12,7 @@ const auth = getAuth(app)
 
 function App() {
 
-  const handleEmailBlue = (event) => {
+  const handleEmailBlur = (event) => {
     console.log(event.target.value)
   }
 
@@ -20,15 +25,31 @@ function App() {
     event.preventDefault();
   }
   return (
-    <div className="App">
+    <div>
 
-      <form onSubmit={handleFormSubmit}>
-        <input onBlur={handleEmailBlue} type="email" name="" id="" />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+      <div className='registration w-50 mx-auto mt-5'>
+        <h2 className='text-primary'>Please Register!!</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
 
     </div>
   );
