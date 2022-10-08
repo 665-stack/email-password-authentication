51f5329aab5fa7efd0023c4c93d3723c3ecfc2d0
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-08 12:23:42 +0600

    email validation using boostrap and password validation using regex

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
+
+
   const handleEmailBlur = (event) => {
     setEmail(event.target.value)
   }
@@ -24,7 +28,25 @@ function App() {
     setPassword(event.target.value)
 
   }
+
+
   const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    // reglar expressoin
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password should contain at least one special character ')
+      return;
+    }
+
+    setValidated(true);
+    setError('');
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(userCredential => {
         const user = userCredential.user;
@@ -36,24 +58,35 @@ function App() {
       })
     event.preventDefault();
   }
+
+
   return (
     <div>
 
       <div className='registration w-50 mx-auto mt-5'>
         <h2 className='text-primary'>Please Register!!</h2>
-        <Form onSubmit={handleFormSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+            <p className='text-danger'>{error}</p>
           </Form.Group>
+
+
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
           </Form.Group>
           <Button variant="primary" type="submit">
